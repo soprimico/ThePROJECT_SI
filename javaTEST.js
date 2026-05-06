a7700ecad7e8c2021f82a7e84b512b8cc28718fb
soprimico <soprimico.com@gmail.com>
2026-05-06 08:19:34 +0200

    lets see

---
+++ javaTEST.js
@@ -0,0 +1,11 @@
+const aran = "notNigga"
+
+if (aran === "notNigga"){
+    print("Aran you're not nigga and u're legit")
+} else {
+    print("Aran u're so nigger")
+}
+
+while (aran === "notNigga") {
+    print("Legit!");
+}
