3172ccd07a49cfc92e22632244975b14b29fcbb8
soprimico <soprimico.com@gmail.com>
2026-05-06 09:26:33 +0200

    opala

--- javaTEST.js
+++ javaTEST.js
@@ -1,11 +1,3 @@
-const aran = "notNigga"
-
-if (aran === "notNigga"){
-    print("Aran you're not nigga and u're legit")
-} else {
-    print("Aran u're so nigger")
-}
-
-while (aran === "notNigga") {
-    print("Legit!");
+function arannigga() {
+    Element.style.backgroundColor = "red";
 }
