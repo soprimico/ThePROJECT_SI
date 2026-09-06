db4488decc1bf7a92e6cf579a433573b0efd2341
bielicus <soprimico.com@gmail.com>
2026-09-06 02:21:35 +0200

    pablo picasso

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ console.log('auth.js loaded correctly');
 // ============================================
 // CONFIGURATION - YOUR LOCAL API
 // ============================================
-const API_URL = 'https://immediate-motorcycles-mind-honey.trycloudflare.com/api/';
+const API_URL = 'https://current-sep-cdna-africa.trycloudflare.com/api/';
 
 // ============================================
 // USER STATE
